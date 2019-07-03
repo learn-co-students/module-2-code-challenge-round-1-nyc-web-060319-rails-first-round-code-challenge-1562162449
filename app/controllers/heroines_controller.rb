d058ea27c141f604c243a95d55a0e36470740a2c
Harpreet Ghotra <harpreetghotra40@icloud.com>
2019-07-03 10:59:25 -0400

    Almost done

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -3,7 +3,28 @@ class HeroinesController < ApplicationController
     @heroines = Heroine.all
   end
 
+  def new
+    @heroine = Heroine.new
+  end
+
+  def create
+    @heroine = Heroine.create(name: params[:heroine][:name], super_name: params[:heroine][:super_name], power_id: params[:heroine][:power_id])
+    if @heroine.valid?
+      @heroine.save
+      redirect_to heroine_path(@heroine)
+    else
+      render :new
+    end
+  end
+
   def show
     @heroine = Heroine.find(params[:id])
+  end
+
+  private
+
+  # params.require(:person).permit(:name, :age)
+  def heroine_params
+    parmas.require(:heroine).permit(:name, :super_name, :power_id)
   end
 end
