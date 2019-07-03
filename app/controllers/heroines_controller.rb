24166c157cc7893a908567dfac1363ba9ec61cdc
github username <github email address>
2019-07-03 11:10:24 -0400

    done with base deliverables

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -8,14 +8,21 @@ class HeroinesController < ApplicationController
   end
 
   def new
+    @heroine = Heroine.new
   end
 
   def create
+    @heroine = Heroine.new(heroine_params)
+    if @heroine.save
+      redirect_to heroine_path(@heroine)
+    else
+      render :new
+    end
   end
 
   private
 
   def heroine_params
-    params.require(:heroines).permit(:name, :super_name, :power_id)
+    params.require(:heroine).permit(:name, :super_name, :power_id)
   end
 end
