f6925995e780cf79aa9a0216c84b7031c4cf5809
Harpreet Ghotra <harpreetghotra40@icloud.com>
2019-07-03 11:03:35 -0400

    Fixed validation error

--- app/models/heroine.rb
+++ app/models/heroine.rb
@@ -1,5 +1,5 @@
 class Heroine < ApplicationRecord
   belongs_to :power
   validates :super_name, uniqueness: :true
-  validate :power_id, presence: :ture
+  validates :power_id, presence: :ture
 end
