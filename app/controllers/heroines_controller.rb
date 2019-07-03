e9d80df735f51fa908a29ee73f75938d01eff12d
github username <github email address>
2019-07-03 10:28:26 -0400

    created associations, show pages, heroine index page

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,20 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  private
+
+  def heroine_params
+    params.require(:heroines).permit(:name, :super_name, :power_id)
+  end
 end
